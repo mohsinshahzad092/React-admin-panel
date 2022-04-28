ee0fa540a934e9ccc6e7a6a9af6ae1e150b633fe
Mohsin Shahzad <72189089+mohsinshahzad092@users.noreply.github.com>
2022-04-28 18:26:20 +0500

    lamadav admin panal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/home/Home.jsx';
+import Login from "./pages/login/Login.jsx";
+import List from "./pages/list/List.jsx";
+import Singal from "./pages/singal/Singal.jsx";
+import New from "./pages/new/New.jsx";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path="usedId" element={<Singal />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path="productId" element={<Singal />} />
+            <Route path="new" element={<New />} />
+          </Route>
+
+        </Route>
+      </Routes>
+    </div >
   );
 }
 
---
+++ src/pages/singal/Singal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Singal = () => {
+  return (
+    <div>Singal</div>
+  )
+}
+
+export default Singal
